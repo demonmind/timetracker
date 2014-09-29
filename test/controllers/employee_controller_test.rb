bca60f3dbb6e8243dd0c692351ecc4f089c1fc05
Kleon Kita <kleon@tests-MacBook-Pro-3.local>
2014-09-29 10:26:10 -0400

    Indenting correctly

--- test/controllers/employee_controller_test.rb
+++ test/controllers/employee_controller_test.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe "Employee", :type => :feature do
 
   it "clock users in" do
-		clockin_employee_general
-
+    clockin_employee_general
     page.should have_content('Clock Out')
   end
 
@@ -17,20 +16,20 @@ describe "Employee", :type => :feature do
   end
 
   it "renders all employee stats if user is admin" do
-  	clockin_employee_admin
+    clockin_employee_admin
 
     page.should have_content('All Employees Stats')
   end
 
   it "renders personal stats if user not an admin" do
-  	clockin_employee_general
+    clockin_employee_general
 
     page.should have_content('My Stats')
     page.should have_content('John')
   end
 
   it "should not render all employee stats if employee not admin" do
-  	clockin_employee_general
+    clockin_employee_general
 
     page.should_not have_content('All Employees Stats')
   end
@@ -48,26 +47,26 @@ describe "Employee", :type => :feature do
   end
 
   feature "Admin add or remove user" do
-	  scenario "should add an employee if uid is present" do
-	  	clockin_employee_admin
-	  	click_link "Add Employee"
-	    fill_in "employee[uid]", :with => "12347"
-	    fill_in "employee[fname]", :with => "Jack"
-	    fill_in "employee[lname]", :with => "nick"
-	    select "Employee", :from => "employee[emp_type]"
-	    click_button "Create"
+     scenario "should add an employee if uid is present" do
+       clockin_employee_admin
+       click_link "Add Employee"
+       fill_in "employee[uid]", :with => "12347"
+       fill_in "employee[fname]", :with => "Jack"
+       fill_in "employee[lname]", :with => "nick"
+       select "Employee", :from => "employee[emp_type]"
+       click_button "Create"
 
-	    page.should have_content('created successfuly!')
-	  end
+       page.should have_content('created successfuly!')
+     end
 
-	  scenario "should delete an employee if pressed delete with employee chosen when logged in as admin" do
-	  	clockin_employee_admin
-	  	click_link "Remove Employee"
-	    select "John", :from => "employee[emp_id]"
-	    click_button "Delete"
+     scenario "should delete an employee if pressed delete with employee chosen when logged in as admin" do
+       clockin_employee_admin
+       click_link "Remove Employee"
+       select "John", :from => "employee[emp_id]"
+       click_button "Delete"
 
-	    page.should have_content('deleted successfuly!')
-	  end
+        page.should have_content('deleted successfuly!')
+      end
   end
 
   def clockin_employee_admin
